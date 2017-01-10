922f4dff035dfc67e2decc6e2759c188b86e0a9c
davidklaver <davidjklaver@gmail.com>
2017-01-09 20:11:08 -0500

    Fix my route

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,6 @@ Rails.application.routes.draw do
 
   get '/examples' => 'examples#index'
 
-  get 'klaverpages' => 'klaver_pages#index'
+  get '/klaverpages' => 'klaver_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
