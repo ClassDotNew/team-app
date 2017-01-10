1431e59862ffa87846f0bde6c1676255f0ca48f5
raychengy <ray.cheng.y@gmail.com>
2017-01-09 19:55:17 -0500

    added a route in the routes.rb file for examples controller index

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   get '/people' => 'people#index'
 
+  get '/examples' => 'examples#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
