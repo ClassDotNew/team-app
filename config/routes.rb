789fe46b5b91c893b7d97dc517c5109755de5361
MattCali <matthewbcalifano@gmail.com>
2017-01-09 20:06:38 -0500

    77777 times i have been rejected

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  get 'examples/index'
+
   get '/pages' => 'pages#index'
+]
+  # Shoes COntroller
+  get '/shoes' => 'shoes#index'
+
   get '/pages/:id' => 'pages#show'
 
+
+  get '/cats' => 'cats#index'
+
+  get '/people' => 'people#index'
+
+  get '/examples' => 'examples#index'
+
+  get 'klaverpages' => 'klaver_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
